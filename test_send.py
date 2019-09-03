cb919b599fce541355aa445ce63b539dde1fd339
b-lakshminarayana <balajimpbl@gmail.com>
2019-09-03 23:03:48 +0530

    Create test_send.py

---
+++ test_send.py
@@ -0,0 +1,17 @@
+import argparse
+from google.cloud import pubsub
+
+
+def list_topics(project_id):
+	#pubsub_client=pubsub.Client()
+	
+for topic in pubsub_client.list_topics():
+	print(topic.name)
+
+
+def create_topic(topic_name):
+	topic_name = "test_topic1"
+	pubsub_client=pubsub.Client()
+	topic = pubsub_client.topic(topic_name)
+	topic.create()
+	print('Topic {} created: {}'.format(topic.name))
